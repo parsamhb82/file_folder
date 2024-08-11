bcc7a4eefdbb82818a7b422d37854b659f304976
parsamhb82 <parsa1382mohebian@gmail.com>
2024-08-11 19:29:26 +0330

    cd commit

--- main.py
+++ main.py
@@ -20,12 +20,8 @@ root_folder = Folder('/')
 this_folder = root_folder
 
 def find_by_directory(directory : str, this_folder : Folder):
-    if directory[0] != '/' and this_folder.get_name() == '/':
-        print('no such file of directory')
-        return False, None, None
-    if this_folder.get_name() == '/':
-        directory_list = directory.split('/')
-        del directory_list[0]
+    direction_folders = []
+    directory_list = directory.split('/')
     current_folder = this_folder
     current_folder : Folder
     for i in range(len(directory_list)):
@@ -35,21 +31,31 @@ def find_by_directory(directory : str, this_folder : Folder):
                 file = current_folder.search_files_by_name(file_whole_name[0], file_whole_name[1])
                 if file == None:
                     print ('no such file or directory')
-                    return False , None, None
-                return True, current_folder, file
+                    return False , None, None, []
+                return True, current_folder, file, direction_folders
             else:
                 current_folder = current_folder.search_folders_by_name(directory_list[i])
                 if current_folder == None:
                     print('no such file or directory')
-                    return False , None , None
-                return True , current_folder , None
+                    return False , None , None, []
+                direction_folders.append(current_folder)
+                return True , current_folder , None, direction_folders
 
             
         else:
             current_folder = current_folder.search_folders_by_name(directory_list[i])
             if current_folder == None:
                 print('no such file or directory')
-                return False , None
+                return False , None, None, []
+            direction_folders.append(current_folder)
+def cd(directory, current_folder,folders_list):
+    found, new_folder, file, direction_folders= find_by_directory(directory, current_folder)
+    if found and file == None:
+        for folder in direction_folders:
+            folders_list.append(folder)
+        return new_folder, folders_list
+    return current_folder, folders_list
+
 
 def text_input():
     print("enter your text (0/end/0 = finish)")
