968dabf9c7c932baa7c919779df6fc6702ee9d53
parsamhb82 <parsa1382mohebian@gmail.com>
2024-08-11 17:36:28 +0330

    main.py text_input

--- main.py
+++ main.py
@@ -11,4 +11,12 @@ def find_by_directory(directory : str, this_folder : Folder):
     if this_folder.get_name() == '/':
         directory_list = directory.split('/')
         del directory_list[0]
+def text_input():
+    print("enter your text (0/end/0 = finish)")
+    inp = ''
+    text = []
+    while inp != '0/end/0':
+        inp = input('')
+        text.append(inp)
+    return text
 
